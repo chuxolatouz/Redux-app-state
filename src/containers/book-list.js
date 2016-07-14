b1077d28e0fefdeef0a341903a2ed32c31a0822c
JesusRincon <00jjessejj00@gmail.com>
2016-07-14 14:53:14 -0400

    Connect to REdux

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,7 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
-  rebderList() {
+class BookList extends Component {
+  renderList() {
     return this.props.books.map((book) => {
       return (
         <li key={book.title} className="list-group-item">
@@ -18,3 +19,11 @@ export default class BookList extends Component {
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
