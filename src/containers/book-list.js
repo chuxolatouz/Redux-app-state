5cb167b51f9644c337a242c7b8823336070b2653
JesusRincon <00jjessejj00@gmail.com>
2016-07-14 16:14:48 -0400

    app finished with simple Redux states

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -7,8 +7,11 @@ class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">
-          {book.title}
+        <li
+        key={book.title}
+        onClick={() => this.props.selectBook(book)}
+        className="list-group-item">
+        {book.title}
         </li>
       );
     });
@@ -29,7 +32,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch){
-  return bindActionCreators({selectBook: selectBook, dispatch});
+  return bindActionCreators({selectBook: selectBook}, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
